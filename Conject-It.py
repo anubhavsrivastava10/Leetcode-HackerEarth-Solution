c57a0561899edf835eaa5496acfff4ff10626eda
Anubhav Srivastava <anubhav.srivastava2707@gmail.com>
2019-12-23 20:32:00 -0800

    favorite question

---
+++ Conject-It.py
@@ -0,0 +1,9 @@
+for _ in range(0,int(input())):
+    print('YES')
+    
+# for every term the output will be 1
+# if N/2 revels even number it will get 1 itself by dividing it by 2
+# if N/2 revels odd number then by the condition 3N+1 will make it even and result 1
+# if the number is odd the condition 3N+1 will make it even and again follow !st and 2nd conditon
+
+# ehnce the output for the given condition will always be 1
