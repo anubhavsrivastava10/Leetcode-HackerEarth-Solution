2922ec2274f359a686406be9ed2b9398ff083e17
Anubhav Srivastava <anubhav.srivastava2707@gmail.com>
2020-01-15 21:43:17 +0530

    Linear Search

--- Wet Clothes.py
+++ Wet Clothes.py
@@ -2,12 +2,14 @@ n, m, g = map(int,input().split())
 time = list(map(int,input().split()))
 arr = list(map(int,input().split()))
 lst = []
-counter= 0
+c= 0
 for i in range(0,n-1):
     x = time[i+1]-time[i]
     lst.append(x)
+    
 m = max(lst)
-for j in range(0,m):
-    if(m>=arr[j]):
-        counter=counter+1
-print(counter)
+c=0
+for i in range(len(arr)):
+    if arr[i]<=m:
+        c+=1
+print(c)
