6019888a1cfc0145a5150695e4dfee8aebda093a
Anubhav Srivastava <anubhav.srivastava2707@gmail.com>
2021-01-03 01:45:57 +0530

    easy set

---
+++ Leetcode/48. Rotate Image.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n//2):
+            for j in range(i,n-i-1):
+                matrix[i][j], matrix[j][n-i-1], matrix[n-i-1][n-j-1], matrix[n-j-1][i] = matrix[n-j-1][i], matrix[i][j], matrix[j][n-i-1], matrix[n-i-1][n-j-1]
