a614ebcb7766301d258063e3e36405e4f438536e
Anubhav Srivastava <anubhav.srivastava2707@gmail.com>
2022-01-04 12:07:20 +0530

    Jan 4 leetcode daily

---
+++ Leetcode/January2022/1009. Complement of Base 10 Integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        val = bin(n)
+        val = val[2:]
+        new = 0
+        for item in val:
+            if item == '1':
+                new *= 10
+                new += 0
+            else:
+                new *= 10
+                new += 1
+        count = 0
+        ans = 0
+        while(new>0):
+            rem = new%10
+            new = new//10
+            ans += (2**count)*rem
+            count+=1
+        return ans
