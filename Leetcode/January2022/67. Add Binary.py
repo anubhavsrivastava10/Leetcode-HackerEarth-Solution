5e57a1a3a2f066272590b7e5980ea466c69fa9db
Anubhav Srivastava <anubhav.srivastava2707@gmail.com>
2022-01-11 13:18:10 +0530

    Jan10 Leetcode daily

---
+++ Leetcode/January2022/67. Add Binary.py
@@ -0,0 +1,14 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        res = ""
+        i, j, carry = len(a) - 1, len(b) - 1, 0
+        while i >= 0 or j >= 0:
+            sum = carry;
+            if i >= 0 : sum += ord(a[i]) - ord('0')
+            if j >= 0 : sum += ord(b[j]) - ord('0')
+            i, j = i - 1, j - 1
+            carry = 1 if sum > 1 else 0;
+            res += str(sum % 2)
+
+        if carry != 0 : res += str(carry);
+        return res[::-1]
