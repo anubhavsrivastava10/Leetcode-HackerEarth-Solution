838c8beec6d98db904464e66460bfcb46c6d44d3
anubhavsrivastava10 <anubhav.srivastava2707@gmail.com>
2022-05-05 10:09:31 +0530

    Updated May 4 Leetcode Daily

--- Leetcode/May 2022/4)1679. Max Number of K-Sum Pairs.py
+++ Leetcode/May 2022/4)1679. Max Number of K-Sum Pairs.py
@@ -1,7 +1,49 @@
-# Will update in O(n)
+# This solution takes O(n) time
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        hashmap = {}
+        count = 0
+        for item in nums:
+            if item in hashmap:
+                if hashmap[item]>0:
+                    count += 1
+                    hashmap[item]-=1
+                else:
+                    final = k-item
+                    if final not in hashmap:
+                        hashmap[final] = 1
+                    else:
+                        hashmap[final] += 1
+            else:
+                final = k-item
+                if final not in hashmap:
+                    hashmap[final] = 1
+                else:
+                    hashmap[final] += 1
+        return count
 
-# This solution takes O(nLog(n))
+# I will just tidy things up in the above solution
+# Instead of finding the array element in the hashmap I will find the difference and store the element
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        hashmap = {}
+        count = 0
+        for item in nums:
+            final = k - item 
+            if final in hashmap:
+                count += 1
+                if hashmap[final]==1:
+                    del hashmap[final]
+                else:
+                    hashmap[final] -= 1
+            else:
+                if item in hashmap:
+                    hashmap[item] += 1
+                else:
+                    hashmap[item] = 1
+        return count
 
+# This solution takes O(nLog(n)) time
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         nums.sort()
